03594c9d6eaf2c3bd31be6c02e018a5041bafd1e
Mozi-k <katsande.moses@gmail.com>
2021-07-04 11:46:39 +0200

    first commit

---
+++ src/test/java/guru/springframework/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,60 @@
+package guru.springframework.repositories;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Optional;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import guru.springframework.bootstrap.RecipeBootstrap;
+import guru.springframework.domain.UnitOfMeasure;
+
+/**
+ * Created by jt on 6/17/17.
+ */
+@RunWith(SpringRunner.class)
+@DataMongoTest
+public class UnitOfMeasureRepositoryIT {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @Autowired
+    RecipeRepository recipeRepository;
+
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        recipeRepository.deleteAll();
+        categoryRepository.deleteAll();
+        unitOfMeasureRepository.deleteAll();
+        
+        RecipeBootstrap recipeBootstrap = new RecipeBootstrap(categoryRepository, recipeRepository, unitOfMeasureRepository);
+        
+        recipeBootstrap.onApplicationEvent(null);
+    }
+
+    @Test
+    public void findByDescription() throws Exception {
+
+        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        assertEquals("Teaspoon", uomOptional.get().getDescription());
+    }
+
+    @Test
+    public void findByDescriptionCup() throws Exception {
+
+        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Cup");
+
+        assertEquals("Cup", uomOptional.get().getDescription());
+    }
+
+}
