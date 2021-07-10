489feb5bb154f93cbd7c6350f88d1084368f4722
Mozi-k <katsande.moses@gmail.com>
2021-07-10 23:10:38 +0200

    Added Multi RouterFunction

--- src/main/java/nexstudio/springframework/config/webConfig.java
+++ src/main/java/nexstudio/springframework/config/webConfig.java
@@ -1,12 +1,13 @@
 package nexstudio.springframework.config;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
 import nexstudio.springframework.model.Recipe;
@@ -15,13 +16,28 @@ import nexstudio.springframework.services.RecipeService;
 @Configuration
 public class webConfig {
 
+    // @Bean
+    // RouterFunction<?> routes(RecipeService recipeService) {
+    //     return RouterFunctions.route(GET("api/recipes"), 
+    //             serverRequest -> ServerResponse
+    //                     .ok()
+    //                     .contentType(MediaType.APPLICATION_JSON)
+    //                     .body(recipeService.getRecipes(), Recipe.class)
+    //         );
+    // }
+
     @Bean
-    RouterFunction<?> routes(RecipeService recipeService) {
-        return RouterFunctions.route(GET("api/recipes"), 
-                serverRequest -> ServerResponse
-                        .ok()
+    RouterFunction<ServerResponse> routes(RecipeService recipeService) {
+        return 
+            route(GET("api/recipes"), 
+                req -> ok()
                         .contentType(MediaType.APPLICATION_JSON)
                         .body(recipeService.getRecipes(), Recipe.class)
-            );
+            )
+            .and(route(GET("api/recipe/{id}"), 
+                req -> ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(recipeService.findById(req.pathVariable("id")), Recipe.class)
+            ));
     }
 }
