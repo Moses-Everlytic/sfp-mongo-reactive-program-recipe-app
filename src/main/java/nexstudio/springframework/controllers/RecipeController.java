195e8bb79c5119d688488cef8d0d1374cbe1e24d
Mozi-k <katsande.moses@gmail.com>
2021-07-10 20:56:21 +0200

    Made Not found Excaption Reactive

--- src/main/java/nexstudio/springframework/controllers/RecipeController.java
+++ src/main/java/nexstudio/springframework/controllers/RecipeController.java
@@ -2,16 +2,21 @@ package nexstudio.springframework.controllers;
 
 import javax.validation.Valid;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.support.WebExchangeBindException;
+import org.thymeleaf.exceptions.TemplateInputException;
 
 import lombok.extern.slf4j.Slf4j;
 import nexstudio.springframework.commands.RecipeCommand;
+import nexstudio.springframework.exceptions.NotFoundException;
 import nexstudio.springframework.services.RecipeService;
 import reactor.core.publisher.Mono;
 
@@ -68,19 +73,16 @@ public class RecipeController {
         return "redirect:/";
     }
 
-    // @ResponseStatus(HttpStatus.NOT_FOUND)
-    // @ExceptionHandler(NotFoundException.class)
-    // public ModelAndView handleNotFound(Exception exception){
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler({NotFoundException.class, TemplateInputException.class})
+    public String handleNotFound(Exception exception, Model model){
 
-    //     log.error("Handling not found exception");
-    //     log.error(exception.getMessage());
+        log.error("Handling not found exception");
+        log.error(exception.getMessage());
 
-    //     ModelAndView modelAndView = new ModelAndView();
+        model.addAttribute("exception", new Exception("Recipe not found"));
 
-    //     modelAndView.setViewName("404error");
-    //     modelAndView.addObject("exception", exception);
-
-    //     return modelAndView;
-    // }
+        return "404error";
+    }
 
 }
